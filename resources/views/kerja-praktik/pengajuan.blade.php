c2c8950599556ac9a9753cee655050505428c1e6
alifnrz27 <alifnrz27@gmail.com>
2022-06-30 15:35:31 +0700

    add new files

---
+++ resources/views/kerja-praktik/pengajuan.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <form action="" enctype="multipart/form-data">
+        @csrf
+        <label for="">input transkrip</label>
+        <input type="text">
+        <label for="">sertif vaksin</label>
+        <input type="text">
+        <label for="">form pengajuan</label>
+        <input type="text">
+    </form>
+</x-app-layout>
